e68f27c5b56acd67ae5de49e97af93c2c72ebf0c
haerisogyumo <haerisogyumo@gmail.com>
2023-01-03 11:50:10 +0900

    2023/01/03 파이썬

---
+++ Ch02/4.기본입출력.py
@@ -0,0 +1,30 @@
+"""
+날짜 : 2023/01/02
+이름 : 진윤희
+내용 : 파이썬 연산자 실습하기
+"""
+
+# 파이썬 입력
+num = input('숫자입력 : ')
+
+# 파이썬 출력
+print('num : ', num)
+print('num type : ',type(num))
+
+# 문자열 숫자 변환
+r1 = int(num) + 1
+print('r1 : ', r1)
+
+
+# 출력옵션
+print('010', '1237', '5678', sep='-')
+print('Hello', end="~ ") #다음 출력문과 한줄로 출력
+print('Python', end=' ')
+print("Programming")
+
+
+# 서식문자
+print('%d년 %d월 %d일 %s요일' % (2023, 1, 2, '월'))
+
+# 포맷문자
+print('{}년 {}월 {}일 {}요일'.format(2023, 1, 2, '월'))
